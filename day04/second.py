23b2d3dd27dfff5a817e09af8f885617d66bb643
scott-olson <scotteolson3@gmail.com>
2019-12-10 14:19:11 -0800

    added some notes to day 04

--- day04/second.py
+++ day04/second.py
@@ -1,4 +1,8 @@
+# interestingsolution inspired by reddit user
+
 def check(n):
+	# first conditional checks to see if the input is in fact sorted; means that the solution is only increasing
+	# second conditional checks that the input has a grouping of exactly 2 of the same digits
     return list(n) == sorted(n) and 2 in map(n.count, n)
 
 print(sum(check(str(n)) for n in range(254032, 789860 + 1)))
